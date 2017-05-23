33656a52c51407c8e2557f4b57ec777c90daccdf
smileHorse <65022492@qq.com>
2017-05-23 15:07:49 +0800

    MFC Windows程序设计

---
+++ MFC Windows程序设计/Ch14/Clock/Clock.h
@@ -0,0 +1,44 @@
+
+class CMyApp : public CWinApp
+{
+public:
+	virtual BOOL InitInstance();
+};
+
+class CMainWindow : public CFrameWnd
+{
+protected:
+	BOOL m_bFullWindow;
+	BOOL m_bStayOnTop;
+
+	int m_nPrevSecond;
+	int m_nPrevMinute;
+	int m_nPrevHour;
+
+	void DrawClockFace(CDC* pDC);
+	void DrawSecondHand(CDC* PDC, int nLength, int nScale, int nDegress, COLORREF clrColor);
+	void DrawHand(CDC* pDC, int nLength, int nScale, int nDegress, COLORREF clrColor);
+
+	void SetTitleBarState();
+	void SetTopMostState();
+	void SaveWindowState();
+	void UpdateSystemMenu(CMenu* pMenu);
+
+public:
+	CMainWindow();
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+	BOOL RestoreWindowState();
+
+protected:
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void OnGetMinMaxInfo(MINMAXINFO* pMMI);
+	afx_msg void OnTimer(UINT nTimerID);
+	afx_msg void OnPaint();
+	afx_msg LRESULT OnNcHitTest(CPoint point);
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint pos);
+	afx_msg void OnEndSession(BOOL bEnding);
+	afx_msg void OnClose();
+
+	DECLARE_MESSAGE_MAP()
+};
